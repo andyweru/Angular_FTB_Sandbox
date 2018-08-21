38693a2acd771b43b14ed01620aa3f1a64ae9700
Andrew Waweru <andyweru@gmail.com>
2018-08-21 14:48:28 +0300

    used ngFor to loop through items and objects in an array

--- src/app/components/sandbox/sandbox.component.ts
+++ src/app/components/sandbox/sandbox.component.ts
@@ -8,6 +8,29 @@ import { Component } from '@angular/core';
 })
 
 export class SandboxComponent{
+    people = ['Rick', 'Daryl', 'Carl', 'Glen'];
+    
+    people2 = [
+        {
+            firstName:'Rick',
+            lastName: 'Grimes'
+        },
+        {
+            firstName:'Daryl',
+            lastName: 'Dixon'
+        },
+        {
+            firstName:'Carl',
+            lastName: 'Grimes'
+        },
+        {
+            firstName:'Glen',
+            lastName: 'Rhee'
+        }
+    ];
 
+    constructor(){
+        this.people[2] = 'Carol';
+    }
 }
 
