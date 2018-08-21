4bbf3cfd293d8cd62919ff49e95d01de69a7c125
Andrew Waweru <andyweru@gmail.com>
2018-08-21 13:40:27 +0300

    exploring the class and template in components

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'sandbox',
-    template:`<h1>Hello World</h1>`
+    template:`
+    <h1>{{ name }} is {{ age }} years old</h1>
+    <h2>My name is {{ person.firstName }} {{ person.lastName }}
+    `
 })
 
 export class SandboxComponent{
-    
+    name = 'John Doe';
+    age = 35;
+    person = {firstName:'Steve', lastName:'Smith'}
+
+    constructor(){
+        console.log('Constructor ran...');
+        // this.age = 36;
+        this.hasBirthday();
+        this.hasBirthday();
+    }
+
+    hasBirthday(){
+        this.age ++;
+    }
 }
